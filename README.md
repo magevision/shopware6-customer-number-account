81c45be5fac407751b152596765a405b75f91d3f
Nikolas <nikolakisae@gmail.com>
2024-01-24 10:59:47 +0200

    Initial commit v1.0.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# [MageVision](https://www.magevision.com/) Customer Number Account for Shopware 6
+
+## Overview
+The Customer Number Account displays customer number in customer account
---
+++ src/MageVisionCustomerNumberAccount.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace MageVision\CustomerNumberAccount;
+
+use Shopware\Core\Framework\Plugin;
+
+class MageVisionCustomerNumberAccount extends Plugin
+{
+}
